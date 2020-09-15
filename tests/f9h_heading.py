dab260b9f03f568c77c8f180ac42d92ded641206
Jirayu Sirivorawong <z5125805@student.unsw.edu.au>
2020-09-15 11:09:41 +0700

    change x-axis to timestamp

--- tests/f9h_heading.py
+++ tests/f9h_heading.py
@@ -6,6 +6,7 @@ from f9p_heading import ask_for_port
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 import itertools
+import datetime  #import time for x-axis
 
 
 x_vals = []
@@ -28,7 +29,7 @@ def plot_heading():
     plt.style.use('dark_background')
     ani = FuncAnimation(plt.gcf(), animate, interval=125)
     
-
+    
     plt.tight_layout()
     plt.show()
 
@@ -40,21 +41,31 @@ def animate(i):
     plt.cla()
     plt.ylabel('degree')
     plt.ylim(0,360)
-    x_vals = x_vals[-100:]
+    x_vals = x_vals[-50:]
 
-    cm = plt.cm.get_cmap('RdYlGn')
-    sc = plt.scatter(x_vals[-100:], y_vals[-100:], c=heading_err[-100:], vmin=80, vmax=100, cmap=cm, s=10) #color scale is reversed
     
+    cm = plt.cm.get_cmap('RdYlGn')
+    plt.scatter(x_vals[-50:], y_vals[-50:], c=heading_err[-50:], vmin=80, vmax=100, cmap=cm, s=10) #color scale is reversed
+    plt.gcf().autofmt_xdate()
     # cbar = plt.colorbar(sc)
     # cbar.ax.set_ylabel('error', rotation=270)
     
+def x_plot(str):
+    if (str is 'iteration'):
+        x_vals.append(next(index))
 
+    if (str is 'time'):
+        now = datetime.datetime.now()
+        x_vals.append(now)
 
 def ubx_cb(msg):
     heading_deg = msg.heading_deg
     heading_acc = msg.accHeading * 1e-5
     r_heading_acc = (abs(heading_acc-180)/180)*100 # revesed heading accuracty
-    x_vals.append(next(index))
+    
+    
+    x_plot('time')  # choose between 'time' and 'iteration' to change x-axis in plot
+
     y_vals.append(heading_deg)
     heading_err.append(r_heading_acc)
 
